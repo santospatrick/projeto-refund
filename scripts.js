5650713e0001c86bdddaf415c819e896be9248d3
Patrick Santos <patrickspteixeira@gmail.com>
2024-05-15 09:56:02 -0300

    adiciona itens na lista

--- scripts.js
+++ scripts.js
@@ -3,6 +3,9 @@ const expense = document.getElementById('expense')
 const category = document.getElementById('category')
 const amount = document.getElementById('amount');
 
+// Seleciona os elementos da lista
+const expensesList = document.querySelector('.expenses-list')
+
 amount.addEventListener('input', function() {
     let value = this.value.replace(/\D/g, '');
     value = Number(value) / 100
@@ -26,5 +29,24 @@ form.addEventListener('submit', function(event) {
         created_at: new Date()
     }
 
-    console.log("🚀 ~ form.addEventListener ~ newExpense:", newExpense)
+    addExpense(newExpense)
 })
+
+function addExpense(newExpense) {
+    try {
+        // Cria item da lista
+        const expenseItem = document.createElement('li')
+        expenseItem.classList.add('expense')
+
+        // Criar o ícone
+        const expenseIcon = document.createElement('img')
+        expenseIcon.setAttribute('src', `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute('alt', newExpense.category_name)
+
+        expenseItem.appendChild(expenseIcon)
+        expensesList.appendChild(expenseItem)
+    } catch (error) {
+        alert('Erro ao adicionar despesa!')
+        console.log(error)
+    }
+}
