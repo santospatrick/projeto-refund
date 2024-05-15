06f370ed17ba14f6db782c364fcb294831a9e946
Patrick Santos <patrickspteixeira@gmail.com>
2024-05-15 09:43:22 -0300

    recupera dados do formulario

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const form = document.querySelector('form')
+const expense = document.getElementById('expense')
+const category = document.getElementById('category')
 const amount = document.getElementById('amount');
 
 amount.addEventListener('input', function() {
@@ -10,3 +13,18 @@ function formatCurrencyBRL(value) {
     value = value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
     return value
 }
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date()
+    }
+
+    console.log("🚀 ~ form.addEventListener ~ newExpense:", newExpense)
+})
