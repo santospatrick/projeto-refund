5796174f25b40868862fb9049464959858aeb62a
Patrick Santos <patrickspteixeira@gmail.com>
2024-05-15 09:32:03 -0300

    remove caracteres indesejados no campo "valor"

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-console.log('scripts.js loaded!')
+const amount = document.getElementById('amount');
+
+amount.addEventListener('input', function() {
+    const value = this.value.replace(/\D/g, '');
+    this.value = new Intl.NumberFormat().format(value);
+})
